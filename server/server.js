3cedae7dab5b45db3bea4f31985626d38e8f7ff5
Lakshay Sharma <lakshay.sharma994@gmail.com>
2018-09-08 07:04:18 -0500

    /PATCH route setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todos');
@@ -70,7 +71,35 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(404).send();
     });
-})
+});
+
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)){
+        console.log('Invalid id');
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime()
+    }
+    else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        return res.status(400).send();
+    })
+});
 
 app.listen(port, () => {
     console.log(`Server listening at port ${port}....`);
