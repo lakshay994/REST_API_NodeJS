1d4214f3f748a89f727e08670a95a6e2c7d61c08
Lakshay Sharma <lakshay.sharma994@gmail.com>
2018-09-10 11:32:53 -0500

    DELETE users/me/token route setup to remove token

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,16 @@ UserSchema.methods.genearateAuthToken = function() {
     });
 };
 
+UserSchema.methods.removeToken = function(token) {
+    let user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {token}
+        }
+    });
+};
+
 UserSchema.statics.findByToken = function(token) {
     let User = this;
     let decoded;
@@ -69,10 +79,31 @@ UserSchema.statics.findByToken = function(token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    let User = this;
+    
+    return User.findOne({email}).then((user) => {
+        if(!user){
+            return Promise.reject();
+        }
+        console.log(password);
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res){
+                    resolve(user);
+                }
+                else{
+                    reject();
+                }
+            });
+        });
+    });
+};
+
 UserSchema.pre('save', function(next) {
     let user = this;
 
-    if(user.isModified()){
+    if(user.isModified('password')){
         bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(user.password, salt, (err, hash) => {
                 user.password = hash;
--- server/server.js
+++ server/server.js
@@ -126,6 +126,26 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+
+    let body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        res.send(user);
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
+});
+
 module.exports = {
     app: app
 }
